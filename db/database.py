ecf2062fe617b5278d41e26f0a141d4b1420f408
TERA <p.tyche92@gmail.com>
2026-02-09 13:58:45 +0900

    fix: database-url env 파일 변수명 변경

--- db/database.py
+++ db/database.py
@@ -9,9 +9,9 @@ from sqlalchemy.orm import Session, sessionmaker
 
 dotenv.load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
