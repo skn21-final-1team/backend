fc54a0e1b96361f1b022cf05e59af35c47fbaa66
reasonableplan <juwon123465@gmail.com>
2026-02-27 10:14:19 +0900

    fix: 디렉토리, 소스 user_id 전면 제거 및 크롬 북마크 string 허용

--- crud/directory.py
+++ crud/directory.py
@@ -7,9 +7,9 @@ from models.directory import DirectoryModel
 
 
 def create_directory(
-    db: Session, title: str, user_id: int, notebook_id: int, parent_id: int | None = None
+    db: Session, title: str, notebook_id: int, parent_id: int | None = None
 ) -> DirectoryModel:
-    db_dir = DirectoryModel(title=title, user_id=user_id, parent_id=parent_id, notebook_id=notebook_id)
+    db_dir = DirectoryModel(title=title, parent_id=parent_id, notebook_id=notebook_id)
     db.add(db_dir)
     db.flush()
     return db_dir
