2e1af4664d8b0bd455c453a704a9d3deb76f46d6
TERA <p.tyche92@gmail.com>
2026-02-27 15:21:59 +0900

    fix: 린트 에러 수정

--- crud/directory.py
+++ crud/directory.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import Session
 from models.directory import DirectoryModel
 
 
-def create_directory(
-    db: Session, title: str, notebook_id: int, parent_id: int | None = None
-) -> DirectoryModel:
+def create_directory(db: Session, title: str, notebook_id: int, parent_id: int | None = None) -> DirectoryModel:
     db_dir = DirectoryModel(title=title, parent_id=parent_id, notebook_id=notebook_id)
     db.add(db_dir)
     db.flush()
